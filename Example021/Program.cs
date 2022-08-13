3d42be4daf36e0f4de19c0c73dfd4c98778f7f10
Nata <muravevantsh@mail.ru>
2022-08-13 13:00:33 +0300

    .\Example021\

---
+++ Example021/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int result=1;
+while (result <=N)
+{
+    Console.WriteLine(result*result*result);
+    result ++;
+}
